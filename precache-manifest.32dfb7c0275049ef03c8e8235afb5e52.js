74de86d5888e6c2aa5c6e93a03efc916e911405f
Ferdiany19 <test@youremail.com>
2019-08-19 20:24:30 +0700

    Updates

---
+++ precache-manifest.32dfb7c0275049ef03c8e8235afb5e52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24122c4ac17ed9325005dfecaf72636b",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "5397fb3b06905ec68c46",
+    "url": "/recipe-app/static/css/main.280c775b.chunk.css"
+  },
+  {
+    "revision": "16e0f37da24fd2418da2",
+    "url": "/recipe-app/static/js/2.3647d5e9.chunk.js"
+  },
+  {
+    "revision": "5397fb3b06905ec68c46",
+    "url": "/recipe-app/static/js/main.ab3b19ff.chunk.js"
+  },
+  {
+    "revision": "0576051d6b7a2efd900c",
+    "url": "/recipe-app/static/js/runtime~main.04da1d28.js"
+  }
+]);
